c38a43b60ee789a24c0e5987c5a773b894d75b1c
MatrexsVigil <worldofro@gmail.com>
2020-03-28 12:48:15 -0400

    version 1.15.2-1.0.0

---
+++ src/main/java/pam/pamhc2foodcore/init/FoodBuilderRegistry.java
@@ -0,0 +1,165 @@
+package pam.pamhc2foodcore.init;
+
+import net.minecraft.item.Food;
+
+public class FoodBuilderRegistry {
+		
+//WHEAT 1
+// FLOURITEM 2
+// DOUGHITEM 3
+public static final Food TOASTITEM = (new Food.Builder()).hunger(5).saturation(1.2F).build();
+public static final Food GRILLEDCHEESEITEM = (new Food.Builder()).hunger(13).saturation(1.2F).build();
+public static final Food CARROTBREADITEM = (new Food.Builder()).hunger(7).saturation(1.2F).build();
+public static final Food PUMPKINBREADITEM = (new Food.Builder()).hunger(7).saturation(1.2F).build();
+// PASTAITEM 4
+public static final Food MACNCHEESEITEM = (new Food.Builder()).hunger(9).saturation(1.2F).build();
+public static final Food NOODLESOUPITEM = (new Food.Builder()).hunger(10).saturation(1.2F).build();
+public static final Food APPLEPIEITEM = (new Food.Builder()).hunger(9).saturation(1.2F).build();
+public static final Food SWEETBERRYPIEITEM = (new Food.Builder()).hunger(7).saturation(1.2F).build();
+public static final Food CHOCOLATECUPCAKEITEM = (new Food.Builder()).hunger(2).saturation(1.2F).build();
+public static final Food CARROTCUPCAKEITEM = (new Food.Builder()).hunger(2).saturation(1.2F).build();
+public static final Food PUMPKINCUPCAKEITEM = (new Food.Builder()).hunger(2).saturation(1.2F).build();
+public static final Food CARAMELCUPCAKEITEM = (new Food.Builder()).hunger(2).saturation(1.2F).build();
+
+// BATTERITEM 4
+public static final Food CHOCOLATECAKEITEM = (new Food.Builder()).hunger(7).saturation(1.2F).build();
+public static final Food CHEESECAKEITEM = (new Food.Builder()).hunger(9).saturation(1.2F).build();
+public static final Food PUMPKINCHEESECAKEITEM = (new Food.Builder()).hunger(12).saturation(1.2F).build();
+public static final Food CARROTCAKEITEM = (new Food.Builder()).hunger(9).saturation(1.2F).build();
+public static final Food CHOCOLATEMUFFINITEM = (new Food.Builder()).hunger(3).saturation(1.2F).build();
+public static final Food CARROTMUFFINITEM = (new Food.Builder()).hunger(3).saturation(1.2F).build();
+public static final Food PUMPKINMUFFINITEM = (new Food.Builder()).hunger(3).saturation(1.2F).build();
+public static final Food CARAMELMUFFINITEM = (new Food.Builder()).hunger(3).saturation(1.2F).build();
+public static final Food DONUTITEM = (new Food.Builder()).hunger(8).saturation(1.2F).build();
+public static final Food CHOCOLATEDONUTITEM = (new Food.Builder()).hunger(9).saturation(1.2F).build();
+public static final Food JELLYDONUTITEM = (new Food.Builder()).hunger(12).saturation(1.2F).build();
+
+//MILK 2
+// BUTTERITEM 3
+public static final Food CHEESEITEM = (new Food.Builder()).hunger(4).saturation(1.2F).build();
+// CREAMITEM 3
+public static final Food YOGURTITEM = (new Food.Builder()).hunger(3).saturation(1.2F).build();
+public static final Food CHOCOLATEYOGURTITEM = (new Food.Builder()).hunger(6).saturation(1.2F).build();
+public static final Food APPLEYOGURTITEM = (new Food.Builder()).hunger(7).saturation(1.2F).build();
+public static final Food SWEETBERRYYOGURTITEM = (new Food.Builder()).hunger(5).saturation(1.2F).build();
+public static final Food PUMPKINYOGURTITEM = (new Food.Builder()).hunger(7).saturation(1.2F).build();
+public static final Food CARAMELYOGURTITEM = (new Food.Builder()).hunger(6).saturation(1.2F).build();
+public static final Food ICECREAMITEM = (new Food.Builder()).hunger(6).saturation(1.2F).build();
+public static final Food CHOCOLATEICECREAMITEM = (new Food.Builder()).hunger(9).saturation(1.2F).build();
+public static final Food CARAMELICECREAMITEM = (new Food.Builder()).hunger(9).saturation(1.2F).build();
+
+//COCOA 1
+public static final Food CHOCOLATEBARITEM = (new Food.Builder()).hunger(2).saturation(1.2F).build();
+// COCOAPOWDERITEM 2
+public static final Food HOTCHOCOLATEITEM = (new Food.Builder()).hunger(7).saturation(1.2F).build();
+public static final Food CHOCOLATEBACONITEM = (new Food.Builder()).hunger(6).saturation(1.2F).build();
+public static final Food CHOCOLATEPUDDINGITEM = (new Food.Builder()).hunger(5).saturation(1.2F).build();
+public static final Food CHOCOLATEROLLITEM = (new Food.Builder()).hunger(9).saturation(1.2F).build();
+
+//EGG 2 
+// MAYONAISEITEM 3
+public static final Food BOILEDEGGITEM = (new Food.Builder()).hunger(3).saturation(1.2F).build();
+public static final Food FRIEDEGGITEM = (new Food.Builder()).hunger(3).saturation(1.2F).build();
+public static final Food SCRAMBLEDEGGITEM = (new Food.Builder()).hunger(7).saturation(1.2F).build();
+
+//APPLE 4
+public static final Food APPLEJUICEITEM = (new Food.Builder()).hunger(9).saturation(1.2F).build();
+public static final Food APPLESMOOTHIEITEM = (new Food.Builder()).hunger(11).saturation(1.2F).build();
+public static final Food APPLEJELLYITEM = (new Food.Builder()).hunger(6).saturation(1.2F).build();
+public static final Food APPLEJELLYTOASTITEM = (new Food.Builder()).hunger(12).saturation(1.2F).build();
+public static final Food APPLESAUCEITEM = (new Food.Builder()).hunger(10).saturation(1.2F).build();
+
+//MELON 2
+public static final Food MELONJUICEITEM = (new Food.Builder()).hunger(5).saturation(1.2F).build();
+public static final Food MELONSMOOTHIEITEM = (new Food.Builder()).hunger(7).saturation(1.2F).build();
+public static final Food MELONJELLYITEM = (new Food.Builder()).hunger(4).saturation(1.2F).build();
+public static final Food MELONJELLYTOASTITEM = (new Food.Builder()).hunger(10).saturation(1.2F).build();
+
+//SWEET BERRY 2
+public static final Food SWEETBERRYJUICEITEM = (new Food.Builder()).hunger(3).saturation(1.2F).build();
+public static final Food SWEETBERRYSMOOTHIEITEM = (new Food.Builder()).hunger(5).saturation(1.2F).build();
+public static final Food SWEETBERRYJELLYITEM = (new Food.Builder()).hunger(3).saturation(1.2F).build();
+public static final Food SWEETBERRYJELLYTOASTITEM = (new Food.Builder()).hunger(9).saturation(1.2F).build();
+
+//CHICKEN 2/6
+public static final Food FRIEDCHICKENITEM = (new Food.Builder()).hunger(7).saturation(1.2F).build();
+public static final Food GROUNDCHICKENITEM = (new Food.Builder()).hunger(3).saturation(1.2F).build();
+public static final Food CHICKENNUGGETITEM = (new Food.Builder()).hunger(3).saturation(1.2F).build();
+public static final Food BASICCHICKENSANDWICHITEM = (new Food.Builder()).hunger(12).saturation(1.2F).build();
+public static final Food CHICKENJERKYITEM = (new Food.Builder()).hunger(5).saturation(1.2F).build();
+public static final Food CHICKENPOTPIEITEM = (new Food.Builder()).hunger(10).saturation(1.2F).build();
+public static final Food CHICKENDINNERITEM = (new Food.Builder()).hunger(21).saturation(1.2F).build();
+
+//BEEF 3/8
+public static final Food POTROASTITEM = (new Food.Builder()).hunger(8).saturation(1.2F).build();
+public static final Food GROUNDBEEFITEM = (new Food.Builder()).hunger(4).saturation(1.2F).build();
+public static final Food BASICHAMBURGERITEM = (new Food.Builder()).hunger(13).saturation(1.2F).build();
+public static final Food BASICCHEESEBURGERITEM = (new Food.Builder()).hunger(17).saturation(1.2F).build();
+public static final Food BACONCHEESEBURGERITEM = (new Food.Builder()).hunger(25).saturation(1.2F).build();
+public static final Food BEEFJERKYITEM = (new Food.Builder()).hunger(6).saturation(1.2F).build();
+
+//PORK 3/8
+public static final Food GROUNDPORKITEM = (new Food.Builder()).hunger(4).saturation(1.2F).build();
+public static final Food PORKJERKYITEM = (new Food.Builder()).hunger(6).saturation(1.2F).build();
+public static final Food HOTDOGITEM = (new Food.Builder()).hunger(13).saturation(1.2F).build();
+public static final Food BACONANDEGGSITEM = (new Food.Builder()).hunger(6).saturation(1.2F).build();
+
+//FISH 2/6
+public static final Food GROUNDFISHITEM = (new Food.Builder()).hunger(3).saturation(1.2F).build();
+public static final Food FISHSTICKSITEM = (new Food.Builder()).hunger(6).saturation(1.2F).build();
+public static final Food BASICFISHSANDWICHITEM = (new Food.Builder()).hunger(12).saturation(1.2F).build();
+public static final Food FISHJERKYITEM = (new Food.Builder()).hunger(5).saturation(1.2F).build();
+public static final Food FISHANDCHIPSITEM = (new Food.Builder()).hunger(11).saturation(1.2F).build();
+
+//MUTTON 2/6
+public static final Food GROUNDMUTTONITEM = (new Food.Builder()).hunger(3).saturation(1.2F).build();
+public static final Food MUTTONJERKYITEM = (new Food.Builder()).hunger(5).saturation(1.2F).build();
+
+//RABBIT 2/5
+public static final Food GROUNDRABBITITEM = (new Food.Builder()).hunger(3).saturation(1.2F).build();
+public static final Food RABBITJERKYITEM = (new Food.Builder()).hunger(5).saturation(1.2F).build();
+
+//POTATO 1/5
+public static final Food BUTTEREDBAKEDPOTATOITEM = (new Food.Builder()).hunger(11).saturation(1.2F).build();
+public static final Food MASHEDPOTATOESITEM = (new Food.Builder()).hunger(11).saturation(1.2F).build();
+public static final Food FRIESITEM = (new Food.Builder()).hunger(6).saturation(1.2F).build();
+public static final Food POTATOCHIPSITEM = (new Food.Builder()).hunger(6).saturation(1.2F).build();
+public static final Food POTATOSOUPITEM = (new Food.Builder()).hunger(7).saturation(1.2F).build();
+
+//CARROT 3
+public static final Food GLAZEDCARROTSITEM = (new Food.Builder()).hunger(8).saturation(1.2F).build();
+public static final Food CARROTSOUPITEM = (new Food.Builder()).hunger(7).saturation(1.2F).build();
+
+//BEET 1
+public static final Food PICKLEDBEETSITEM = (new Food.Builder()).hunger(7).saturation(1.2F).build();
+
+//PUMPKIN 3
+public static final Food PUMPKINSOUPITEM = (new Food.Builder()).hunger(7).saturation(1.2F).build();
+
+//SUGAR 1
+public static final Food CARAMELITEM = (new Food.Builder()).hunger(4).saturation(1.2F).build();
+public static final Food CARAMELAPPLEITEM = (new Food.Builder()).hunger(9).saturation(1.2F).build();
+public static final Food MARSHMELLOWSITEM = (new Food.Builder()).hunger(2).saturation(1.2F).build();
+public static final Food MARSHMELLOWCHICKSITEM = (new Food.Builder()).hunger(3).saturation(1.2F).build();
+public static final Food COTTONCANDYITEM = (new Food.Builder()).hunger(2).saturation(1.2F).build();
+
+//MISC
+// COOKINGOILITEM 2
+// VINEGARITEM 5
+// FRESHWATERITEM 2
+// FRESHMILKITEM 1
+// SALTITEM 2
+public static final Food STOCKITEM = (new Food.Builder()).hunger(5).saturation(1.2F).build();
+
+//GENERIC FOOD
+public static final Food FRUITPUNCHITEM = (new Food.Builder()).hunger(9).saturation(1.2F).build();
+public static final Food STEWITEM = (new Food.Builder()).hunger(15).saturation(1.2F).build();
+public static final Food BAKEDVEGETABLEMEDLYITEM = (new Food.Builder()).hunger(7).saturation(1.2F).build();
+public static final Food FRUITSALADITEM = (new Food.Builder()).hunger(9).saturation(1.2F).build();
+public static final Food MEATLOAFITEM = (new Food.Builder()).hunger(12).saturation(1.2F).build();
+public static final Food P8JUICEITEM = (new Food.Builder()).hunger(7).saturation(1.2F).build();
+public static final Food FRUITCRUMBLEITEM = (new Food.Builder()).hunger(10).saturation(1.2F).build();
+
+	
+
+}
